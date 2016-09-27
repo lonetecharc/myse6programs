9ede6767521ed9227f1e8e0f89882a816e749d9c
Bappa <bappa@MACLTAMB00131.local>
2016-09-27 19:30:18 +0530

    stage 2

---
+++ gulpfile.babel.js
@@ -0,0 +1,49 @@
+import gulp from 'gulp';
+import shell from 'gulp-shell';
+import rimraf from 'rimraf';
+import run from 'run-sequence';
+import watch from 'gulp-watch';
+import server from 'gulp-live-server';
+
+const paths = {
+    js:['./src/**/*.js'],
+    destination: './app'
+};
+
+gulp.task('default', cb => {
+    run('server','build','watch',cb);
+});
+
+gulp.task('clean', cb => {
+    rimraf(paths.destination,cb);
+});
+
+gulp.task('flow',shell.task([
+    'flow'
+],{ ignoreErrors:true }));
+
+gulp.task('babel',shell.task([
+    'babel src --out-dir app'
+]));
+
+
+
+gulp.task('build', cb => {
+    run('clean','flow', 'babel', 'restart', cb);
+});
+
+gulp.task('watch',() => {
+    return watch(paths.js,()=>{
+        gulp.start('build');
+    });
+});
+
+let express;
+
+gulp.task('server',() => {
+    express = server.new(paths.destination);
+});
+
+gulp.task('restart',() => {
+    express.start.bind(express)();
+});
